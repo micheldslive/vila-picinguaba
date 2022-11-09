cf067c6c34556c2cdccd96de186fab62423a83b1
Michel Domingos <michel.silva@dotgroup.com.br>
2022-11-09 19:19:18 -0300

    feat: adding general root items

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,7 +1,28 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import tsConfigPaths from 'vite-tsconfig-paths'
+import EnvironmentPlugin from 'vite-plugin-environment'
+import pkg from 'vite-plugin-linter'
+import svgr from 'vite-plugin-svgr'
+
+const { EsLinter, linterPlugin } = pkg;
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()]
-})
+export default defineConfig((configEnv) => ({
+  plugins: [
+    react(),
+    svgr(),
+    tsConfigPaths(),
+    EnvironmentPlugin('all'),
+    linterPlugin({
+      include: ['./src/**/*.{ts,tsx}'],
+      linters: [new EsLinter({ configEnv })],
+    }),
+  ],
+  test: {
+    globals: true,
+    environment: 'jsdom',
+    setupFiles: '.vitest//setupTests.ts',
+    css: true,
+  },
+}))

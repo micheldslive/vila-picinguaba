378cc6700e7d0849781cafca9441d154f8e731d2
Michel Domingos <michel.silva@dotgroup.com.br>
2022-11-09 08:36:49 -0300

    feat: adding code to main and deleting default files

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,8 +1,25 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { NestFactory } from '@nestjs/core'
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger'
+import { AppModule } from './app.module'
 
-async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+const bootstrap = async () => {
+  const app = await NestFactory.create(AppModule)
+  const port: any = process.env.PORT || 4000
+
+  const APP_NAME = process.env.npm_package_name || 'api-picinguaba'
+  const APP_VERSION = process.env.npm_package_version || '1.0'
+
+  const options = new DocumentBuilder()
+    .setTitle(APP_NAME)
+    .setDescription(
+      `Bem vindo(a) a ${APP_NAME}, API do teste prático do Gama Xp 39. Para consultar mais informações, acesse nossa documentação abaixo.`,
+    )
+    .setVersion(APP_VERSION)
+    .build()
+  const document = SwaggerModule.createDocument(app, options)
+  SwaggerModule.setup('/', app, document)
+
+  await app.listen(port)
 }
-bootstrap();
+
+bootstrap()

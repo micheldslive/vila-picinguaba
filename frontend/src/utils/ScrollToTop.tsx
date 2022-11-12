0b318958559c72b75f351f87641a3e3da22e820b
Michel Domingos <michel.silva@dotgroup.com.br>
2022-11-12 16:31:39 -0300

    chore: lint:fix

--- frontend/src/utils/ScrollToTop.tsx
+++ frontend/src/utils/ScrollToTop.tsx
@@ -1,18 +1,18 @@
-import { useState, useEffect } from "react";
-import { useLocation } from "react-router-dom";
+import { useState, useEffect } from 'react'
+import { useLocation } from 'react-router-dom'
 
 const ScrollToTop = () => {
-  const location = useLocation();
-  const [current, setCurrent] = useState(location.pathname);
+  const location = useLocation()
+  const [current, setCurrent] = useState(location.pathname)
 
   useEffect(() => {
     if (current !== location.pathname) {
-      window.scrollTo(0, 0);
-      setCurrent(location.pathname);
+      window.scrollTo(0, 0)
+      setCurrent(location.pathname)
     }
-  }, [location, current]);
+  }, [location, current])
 
-  return null;
-};
+  return null
+}
 
-export default ScrollToTop;
+export default ScrollToTop
--- frontend/src/utils/gsapEffect.ts
+++ frontend/src/utils/gsapEffect.ts
@@ -1,64 +1,59 @@
-import gsap from "gsap";
-import { ScrollTrigger } from "gsap/ScrollTrigger";
-import React from "react";
-gsap.registerPlugin(ScrollTrigger);
-
-interface Element {
-  element: React.MouseEvent | undefined
-}
+import gsap from 'gsap'
+import { ScrollTrigger } from 'gsap/ScrollTrigger'
+gsap.registerPlugin(ScrollTrigger)
 
 export const gsapFadeMove = (element: gsap.DOMTarget) => {
   const animate = gsap.fromTo(
     element,
     { opacity: 0, y: 50 },
-    { opacity: 1, y: 0, duration: 1 }
-  );
+    { opacity: 1, y: 0, duration: 1 },
+  )
   ScrollTrigger.create({
     trigger: element,
     animation: animate,
-    toggleActions: "play none none none",
+    toggleActions: 'play none none none',
     once: true,
-  });
-};
+  })
+}
 
 export const gsapFadeTop = (element: gsap.DOMTarget) => {
   const animate = gsap.fromTo(
     element,
     { opacity: 0 },
-    { opacity: 1, duration: 1 }
-  );
+    { opacity: 1, duration: 1 },
+  )
   ScrollTrigger.create({
     trigger: element,
     animation: animate,
-    toggleActions: "play none none none",
+    toggleActions: 'play none none none',
     once: true,
-  });
-};
+  })
+}
 
 export const gsapMoveLeft = (element: gsap.DOMTarget) => {
   const animate = gsap.to(element, {
     xPercent: -50,
-    ease: "none",
-  });
+    ease: 'none',
+  })
   ScrollTrigger.create({
     trigger: element,
     animation: animate,
-    start: "top top-=200",
-    end: "top top+=300",
-    scrub: 1
-  });
-};
+    start: 'top top-=200',
+    end: 'top top+=300',
+    scrub: 1,
+  })
+}
 
 export const gsapMoveRight = (element: gsap.DOMTarget) => {
   const animate = gsap.to(element, {
     xPercent: -100,
-    ease: "none",
-  });
+    ease: 'none',
+  })
   ScrollTrigger.create({
     trigger: element,
     animation: animate,
-    start: "top top+=100",
-    end: "top top-=100",
-    scrub: 1
-  });
-};
+    start: 'top top+=100',
+    end: 'top top-=100',
+    scrub: 1,
+  })
+}

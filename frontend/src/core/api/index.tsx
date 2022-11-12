f6001ab0b836ad2ee271b6c36bf0ec3ec6c8c8a6
Michel Domingos <michel.silva@dotgroup.com.br>
2022-11-12 16:29:29 -0300

    refactor(core): improvements in api and states methods

--- frontend/src/core/api/index.tsx
+++ frontend/src/core/api/index.tsx
@@ -1,14 +1,39 @@
 import axios from 'axios'
 
+const baseURL = import.meta.env.VITE_API_BASEURL
+
 const config = {
-  baseURL: import.meta.env.VITE_API_BASEURL || '',
+  baseURL,
 }
 
 const api = axios.create(config)
 
 export default {
-  getProducts: (id: string) =>
-    api({ method: 'GET', url: `produtos/categoria/${id}` }),
+  async getProducts() {
+    const get = (id: string) =>
+      api({ method: 'GET', url: `/produtos/categoria/${id}` })
 
-  getProduct: (id: string) => api({ method: 'GET', url: `produtos/${id}` }),
+    const {
+      VITE_ID_EVENTOS,
+      VITE_ID_ROTEIROS,
+      VITE_ID_ACOMODACAO,
+      VITE_ID_ALIMENTACAO,
+    } = import.meta.env
+
+    const eventos = get(VITE_ID_EVENTOS)
+    const roteiros = get(VITE_ID_ROTEIROS)
+    const acomodacao = get(VITE_ID_ACOMODACAO)
+    const alimentacao = get(VITE_ID_ALIMENTACAO)
+
+    return await Promise.allSettled([
+      eventos,
+      roteiros,
+      acomodacao,
+      alimentacao,
+    ])
+  },
+
+  getProduct(id: string = '2327918a-748d-4a9b-af77-3807500589d6') {
+    return api({ method: 'GET', url: `/produtos/${id}` })
+  },
 }

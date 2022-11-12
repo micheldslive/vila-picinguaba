203511ae34791cd0b6328ea1539bb762f11d6a1c
Michel Domingos <michel.silva@dotgroup.com.br>
2022-11-12 16:35:33 -0300

    chore: adding api call

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,13 +1,21 @@
-import { BrowserRouter as Router } from "react-router-dom";
-import { HelmetProvider } from "react-helmet-async";
-import Preloader from "@/components/preloader";
-import Header from "@/components/header";
-import { Padding } from "@/assets/styles/main";
-import Routers from "@/routes";
-import Footer from "@/components/footer";
-import ScrollToTop from "@/utils/ScrollToTop";
+import { BrowserRouter as Router } from 'react-router-dom'
+import { HelmetProvider } from 'react-helmet-async'
+import Preloader from '@/components/preloader'
+import Header from '@/components/header'
+import { Padding } from '@/assets/styles/main'
+import Routers from '@/routes'
+import Footer from '@/components/footer'
+import ScrollToTop from '@/utils/ScrollToTop'
+import { useEffect } from 'react'
+import { useDataStore } from './core/zustand'
 
 const App = () => {
+  const { setData } = useDataStore()
+
+  useEffect(() => {
+    setData()
+  }, [setData])
+
   return (
     <Router>
       <Preloader />
@@ -19,7 +27,7 @@ const App = () => {
       </HelmetProvider>
       <Footer />
     </Router>
-  );
-};
+  )
+}
 
-export default App;
+export default App
